5a42d703bf91802123bbbaefb30fffe2b956c80f
Shadowblades746 <77968152+Shadowblades746@users.noreply.github.com>
2022-12-29 10:19:18 +0000

    initial commit

---
+++ test_project.py
@@ -0,0 +1,15 @@
+from project import good_morning
+from project import good_afternoon
+from project import good_evening
+
+
+def test_good_morning():
+    assert good_morning("good") == "good morning"
+
+
+def test_good_afternoon():
+    assert good_afternoon("good") == "good afternoon"
+
+
+def test_good_evening():
+    assert good_evening("good") == "good evening"
